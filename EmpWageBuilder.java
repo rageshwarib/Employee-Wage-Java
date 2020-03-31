d8f6fdde4bf1bc9b98aae7f29833759a41ad0a1e
Rageshwari Bodke <61499788+rageshwarib@users.noreply.github.com>
2020-03-31 20:44:47 +0530

    Calculating daily Employee Wage

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -1,19 +1,19 @@
 // Welcome to Employee Wage problem
 public class EmpWageBuilder {
    public static void main(String[] args) {
-		// Constants
+      // Constants
       int IS_FULL_TIME = 1;
-		int EMP_RATE_PER_HOUR = 20;
-		// Variables
-		int empHrs = 0;
-		int empWage = 0;
-		// Computation
+      int EMP_RATE_PER_HOUR = 20;
+      // Variables
+      int empHrs = 0;
+      int empWage = 0;
+      // Computation
       double empCheck = Math.floor(Math.random() * 10) % 2;
       if (empCheck == IS_FULL_TIME)
-			empHrs = 8;
+         empHrs = 8;
       else
-			empHrs = 0;
-			empWage = empHrs * EMP_RATE_PER_HOUR;
-			System.out.println("Employee Wage: " + empWage);
-      }
+         empHrs = 0;
+      empWage = empHrs * EMP_RATE_PER_HOUR;
+      System.out.println("Employee Wage: " + empWage);
+   }
 }
