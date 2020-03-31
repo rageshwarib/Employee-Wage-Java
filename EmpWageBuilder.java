76fbf5fec6d08402aa0bff7ab1da2ea6c865d417
Rageshwari Bodke <61499788+rageshwarib@users.noreply.github.com>
2020-03-31 20:49:02 +0530

    Adding part time Employee using If Statement

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -1,22 +1,22 @@
 // Welcome to Employee Wage problem
 public class EmpWageBuilder {
    public static void main(String[] args) {
-		// Constants
-		int IS_PART_TIME = 1;
+      // Constants
+      int IS_PART_TIME = 1;
       int IS_FULL_TIME = 2;
-		int EMP_RATE_PER_HOUR = 20;
-		// Variables
-		int empHrs = 0;
-		int empWage = 0;
-		// Computation
+      int EMP_RATE_PER_HOUR = 20;
+      // Variables
+      int empHrs = 0;
+      int empWage = 0;
+      // Computation
       double empCheck = Math.floor(Math.random() * 10) % 3;
       if (empCheck == IS_PART_TIME)
-			empHrs = 4;
-		else if (empCheck == IS_FULL_TIME)
-			empHrs = 8;
+         empHrs = 4;
+      else if (empCheck == IS_FULL_TIME)
+         empHrs = 8;
       else
-			empHrs = 0;
-		empWage = empHrs * EMP_RATE_PER_HOUR;
-		System.out.println("Employee Wage: " + empWage);
-	}
+         empHrs = 0;
+      empWage = empHrs * EMP_RATE_PER_HOUR;
+      System.out.println("Employee Wage: " + empWage);
+   }
 }
